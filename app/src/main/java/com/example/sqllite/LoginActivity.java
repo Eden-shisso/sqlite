92446664ea1ce6b7d6e0a89a299ee1d9a5f31262
root <shissomoboni@gmail.com>
2019-05-17 16:05:38 +0300

    sqlite

---
+++ app/src/main/java/com/example/sqllite/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.example.sqllite;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText e1,e2;
+    Button b1;
+    DatabaseHelper db;
+    TextView et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        db = new DatabaseHelper(this);
+
+        e1=findViewById(R.id.emai);
+        e2=findViewById(R.id.editText2);
+        b1=findViewById(R.id.button);
+
+        et=findViewById(R.id.texregister);
+        et.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=e1.getText().toString();
+                String password=e2.getText().toString();
+                Boolean chkemailpass =db.emailpassword(email,password);
+                if (chkemailpass==true)
+                    Toast.makeText(getApplicationContext(),"successful login",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),"Wrong email or password",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sqllite/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.sqllite;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //calling the database
+    DatabaseHelper db;
+
+    EditText e1,e2,e3;
+    Button b1,b2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //call database to the projectv initialize
+        db=new DatabaseHelper(this);
+
+        e1=findViewById(R.id.email);
+        e2=findViewById(R.id.password);
+        e3=findViewById(R.id.cpass);
+        b1=findViewById(R.id.register);
+        b2=findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=e1.getText().toString();
+                String s2=e2.getText().toString();
+                String s3=e3.getText().toString();
+
+                //database phase 3
+                if (s1.equals("")||s2.equals("")||s3.equals("")){
+                    Toast.makeText(getApplicationContext(),"Fields are empty",Toast.LENGTH_SHORT).show();
+
+                }else {
+                    if (s2.equals(s3)){
+                        Boolean chkemail=db.chkemail(s1);
+                        if (chkemail==true){
+                            Toast.makeText(getApplicationContext(),"Resgistration Sucessfull",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Email Already Exist",Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+                Toast.makeText(getApplicationContext(),"Password do not mach",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
